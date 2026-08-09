9e72ae94868cc5603da139f547d50716e1776e0e
Abner Cruz <64279184+AbnerCruz@users.noreply.github.com>
2026-08-09 03:29:13 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'calc-aurica-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-512-maskable.png'
+];
+
+self.addEventListener('install', (e)=>{
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
+    ).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e)=>{
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fetchPromise = fetch(e.request).then(networkResp => {
+        if(networkResp && networkResp.status===200 && e.request.method==='GET'){
+          const clone = networkResp.clone();
+          caches.open(CACHE).then(cache=>cache.put(e.request, clone));
+        }
+        return networkResp;
+      }).catch(()=>cached);
+      return cached || fetchPromise;
+    })
+  );
+});
